d7c7f1262a574561fc6db6d1bd4890c7b968ad4e
AndreasLF <andreas@lyhnefiehn.dk>
2018-01-30 10:41:55 +0100

    CHANGE: Gets the data from every "skemabrik"-element
    Gets the "data-additionalinfo"-attribute for each element with the
    s2skemabrik class
    
    POC:
    Echoed "data-additionalinfo" values to screen.

--- scrape.php
+++ scrape.php
@@ -5,7 +5,10 @@ include('simple_html_dom.php');
 //creates html-DOM from the URL
 $html = file_get_html('https://www.lectio.dk/lectio/681/SkemaNy.aspx?type=elev&elevid=14742506655'); 
 
-echo $html;
-
+foreach($html->find('.s2skemabrik') as $element){
+    $data = $element->getAttribute('data-additionalinfo');
+    echo $data
+    }
+    
 
 ?>
