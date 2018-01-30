4a69821412d4f817e16181e9fd71862bb9d73a5b
AndreasLF <andreas@lyhnefiehn.dk>
2018-01-30 10:57:51 +0100

    CHANGE: Checks if the class has been changed or cancelled
    Performs a regular expressions to check for the strings "Ændret" or
    "Changed"
    
    POC:
    Echoes the result

--- scrape.php
+++ scrape.php
@@ -5,10 +5,28 @@ include('simple_html_dom.php');
 //creates html-DOM from the URL
 $html = file_get_html('https://www.lectio.dk/lectio/681/SkemaNy.aspx?type=elev&elevid=14742506655'); 
 
+$lessonStatus = "";
+
 foreach($html->find('.s2skemabrik') as $element){
     $data = $element->getAttribute('data-additionalinfo');
-    echo $data
+   
+    
+    //Peforms af regular expression to check if the class has been changed
+    if(preg_match('/^Ændret!/',$data)){
+             
+        //LessonStatus changed
+        $lessonStatus = 'Ændret';
+            
     }
+    //Checks if the class has been cancelled by performing a regex
+    else if(preg_match('/^Aflyst!/', $data)){
+        
+        //LessonStaus is changed
+        $lessonStatus = 'Aflyst';
+    }
+   
+    
+}
     
 
 ?>
