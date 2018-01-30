360a593ccfb14e3421b1ab14e90bacbb2c3c631d
AndreasLF <andreas@lyhnefiehn.dk>
2018-01-30 12:17:58 +0100

    CHANGE: Gets the notes and homework
    Performs a regex to get notes and homework.
    Only perform regex on strings that contains a date in the preffered
    format. Some of the elements with the s2skemabrik are empty.
    
    POC:
    var dumps

--- scrape.php
+++ scrape.php
@@ -1,69 +1,92 @@
 <?php
 //includes simple_html_dom library
-include('simple_html_dom.php'); 
+include('simple_html_dom.php');
 
 //creates html-DOM from the URL
-$html = file_get_html('https://www.lectio.dk/lectio/681/SkemaNy.aspx?type=elev&elevid=14742506655'); 
+$html = file_get_html('https://www.lectio.dk/lectio/681/SkemaNy.aspx?type=elev&elevid=14742506655');
 
+//Saves the lesson's staus
 $lessonStatus = "";
 
 foreach($html->find('.s2skemabrik') as $element){
     $data = $element->getAttribute('data-additionalinfo');
-   
-    
+       
     //Peforms af regular expression to check if the class has been changed
-    if(preg_match('/^Ændret!/',$data)){
-             
-        //LessonStatus changed
-        $lessonStatus = 'Ændret';
-        echo $lessonStaus;
-            
-    }
-    //Checks if the class has been cancelled by performing a regex
-    else if(preg_match('/^Aflyst!/', $data)){
+    if(!(preg_match('/(\d\d|\d)\/(\d\d|\d)-(\d\d\d\d)/', $data, $dato)==0)){
+       
+        echo "------------------------------------------------------------------------ <br>";
+        echo $data . "<br>";
+        echo "------------------------------------------------------------------------ <br>";
         
-        //LessonStaus is changed
-        $lessonStatus = 'Aflyst';
-        echo $lessonStaus;
-    }
-    
-    
-    //Dato
-    preg_match('/(\d\d|\d)\/(\d\d|\d)-(\d\d\d\d)/', $data, $dato);
-    print_r($dato);
-    var_dump($dato);
+        
+         //Peforms af regular expression to check if the class has been changed
+        if(preg_match('/^Ændret!/',$data)){
+             
+             //Change lesson status
+            $lessonStatus = 'Ændret';
+            
+            //If there is a string between "Ændret!" and the date. Here the class descritption will show up if there is any
+            if(preg_match('/^Ændret!\s(.*)\s((\d\d|\d)\/(\d\d|\d)-(\d\d\d\d))/', $data, $description)){
+            print_r($description);
+            var_dump($description); 
+             }
+            
+        }
+        //Checks if the class has been cancelled by performing a regex
+        else if(preg_match('/^Aflyst!/', $data)){
+            $lessonStatus = 'Aflyst';
+            
+            //If there is a string between "Aflyst!" and the date. Here the class descritption will show up if there is any.
+            if(preg_match('/^Aflyst!\s(.*)\s((\d\d|\d)\/(\d\d|\d)-(\d\d\d\d))/', $data, $description)){
+            print_r($description);
+            var_dump($description); 
+            }
+        }
+        //Else if there is a string before the date. Here the class description will show up if there is any.
+        else if(preg_match('/(.*)\s((\d\d|\d)\/(\d\d|\d)-(\d\d\d\d))/', $data, $description)){
+            print_r($description);
+            var_dump($description);
+        }
+        
+        
+        //Date
+        preg_match('/(\d\d|\d)\/(\d\d|\d)-(\d\d\d\d)/', $data, $dato);
+        print_r($dato);
+        var_dump($dato);
         
 
-    //Tid
-    preg_match('/(\d\d:\d\d)\stil\s(\d\d:\d\d)/', $data, $time);
-    print_r($time);
-    var_dump($time);   
+        //Time
+        preg_match('/(\d\d:\d\d)\stil\s(\d\d:\d\d)/', $data, $time);
+        print_r($time);
+        var_dump($time);   
         
    
-    //Class
-    preg_match('/Hold:\s(.*?)\sLærer/', $data, $class);
-    print_r($class);
-    var_dump($class);
-   
-    //Teacher
-    if(preg_match('/Lærer:\s(.*?)\s\((.*?)\)\sLokale/', $data, $teacher)){
-        print_r($teacher);
-        var_dump($teacher);         
-    }else if(preg_match('/Lærere:\s(.*?)\sLokale/', $data, $teacherTemp)){
-        //preg_match('/([^,\s]+)/', $teacherTemp[1], $teacher);
-        $teacher=preg_split("/[,\s]+/",$teacherTemp[1]);
+        //Class
+        preg_match('/Hold:\s(.*?)\sLærer/', $data, $class);
+        print_r($class);
+        var_dump($class);
+        
+        
+       
+
+        //Teacher
+        if(preg_match('/Lærer:\s(.*?)\s\((.*?)\)\sLokale/', $data, $teacher)){
+            print_r($teacher);
+            var_dump($teacher);         
+        }else if(preg_match('/Lærere:\s(.*?)\sLokale/', $data, $teacherTemp)){
+            $teacher=preg_split("/[,\s]+/",$teacherTemp[1]);
             
-        print_r($teacher);
-        var_dump($teacher);
-    } 
-    
-    
-    
-    //Room
-    if(preg_match('/Lokale\S+\s(.*)/', $data, $room)){
+            print_r($teacher);
+            var_dump($teacher);
+        } 
+        
+        
+
+        //Room
+        if(preg_match('/Lokale\S+\s(.*)/', $data, $room)){
         print_r($room);
         var_dump($room);
-        
+            
             //Sorts the string if there is a note or homework
             if(preg_match('/(.*?)\sNote/', $room[1],$roomSorted)){
                 print_r($roomSorted);
@@ -73,10 +96,23 @@ foreach($html->find('.s2skemabrik') as $element){
                 print_r($roomSorted);
                 var_dump($roomSorted);
             }
-    }
+        }
      
-    
+        
+
+        //Lektier
+        if(preg_match('/Lektier:\s-\s(.*)/', $data, $homework)){
+            print_r($homework);
+            var_dump($homework);
+        }
+        
+        //Note
+        if(preg_match('/Note:\s(.*)/', $data, $note)){
+            print_r($note);
+            var_dump($note);      
+        }
+    }
 }
-    
+
 
 ?>
