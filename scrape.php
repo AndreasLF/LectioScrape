f801af8358ca3c74398c19f4d5b88ba176731c96
AndreasLF <andreas@lyhnefiehn.dk>
2018-01-31 18:21:55 +0100

    CHANGE: Added database connection

--- scrape.php
+++ scrape.php
@@ -1,6 +1,11 @@
 <?php
 //includes simple_html_dom library
-include('simple_html_dom.php');
+require('simple_html_dom.php');
+
+//Includes connection.php - connects to database
+include('connection.php');
+
+
 //creates html-DOM from the URL
 $html = file_get_html('https://www.lectio.dk/lectio/681/SkemaNy.aspx?type=elev&elevid=14742506655');
 
@@ -168,5 +173,7 @@ foreach($html->find('.s2skemabrik') as $element){
         
     }
 }
+
+
 
 ?>
