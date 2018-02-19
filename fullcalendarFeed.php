e6c3a2a82321a520b6e1a9f28a269afb187b34ff
AndreasLF <andreas@lyhnefiehn.dk>
2018-02-19 20:02:00 +0100

    CHANGE: JSON event-feed created and events added
    JSON event-feed created and fetched by the fullcalendar javaScript
    
    POC:
    Events displayed successfully on the calendar.

---
+++ fullcalendarFeed.php
@@ -0,0 +1,34 @@
+<?php
+$eventList = array();
+
+
+$eventList[] =   array(
+    'title'=>'TEST1',
+    'allday'=>false,
+    'borderColor'=>'#429ef4',
+    'color'=>'#429ef4',
+    'textColor'=>'#000000',
+    'start'=>'2018-02-19T08:15:00',
+    'end'=>'2018-02-19T09:15:00');
+
+$eventList[] =   array(
+    'title'=>'TEST2',
+    'allday'=>false,
+    'borderColor'=>'#f20966',
+    'color'=>'#f20966',
+    'textColor'=>'#000000',
+    'start'=>'2018-02-19T09:20:00',
+    'end'=>'2018-02-19T10:20:00');
+
+$eventList[] =   array(
+    'title'=>'TEST3',
+    'allday'=>true,
+    'borderColor'=>'#f20966',
+    'color'=>'#f20966',
+    'textColor'=>'#000000',
+    'start'=>'2018-02-19');
+
+header('Content-Type: application/json');
+//header('Content-Type: application/json');
+echo json_encode($eventList);
+?>
